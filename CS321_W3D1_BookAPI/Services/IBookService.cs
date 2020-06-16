2980ecc0da3804b372728f28aacb8b598c4bd175
kemora08 <kevin_mora89@hotmail.com>
2020-06-15 20:00:54 -0500

    Initial Model

---
+++ CS321_W3D1_BookAPI/Services/IBookService.cs
@@ -0,0 +1,26 @@
+﻿using CS321_W3D1_BookAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CS321_W3D1_BookAPI.Services
+{
+    public interface IBookService
+    {
+        // Get all books
+        IEnumerable<Book> GetAll();
+
+        // Get a book by id
+        Book Get(int id);
+
+        // Add a new book
+        Book Add(Book newBook);
+
+        // Update a book
+        Book Update(Book updateBook);
+
+        // Delete a book
+        void Delete(Book deletebook);
+    }
+}
