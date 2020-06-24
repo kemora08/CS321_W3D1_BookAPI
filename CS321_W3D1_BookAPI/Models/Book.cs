453ed6106d335393ad3e54998ad8aa7ad20e9af3
kemora08 <kevin_mora89@hotmail.com>
2020-06-23 19:37:18 -0500

    Initial Commit

--- CS321_W3D1_BookAPI/Models/Book.cs
+++ CS321_W3D1_BookAPI/Models/Book.cs
@@ -6,13 +6,16 @@ using System.Threading.Tasks;
 
 namespace CS321_W3D1_BookAPI.Models
 {
-    public class Book
+    public class BookModel
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public int AuthorId { get; set; }
         // Book has an Author
         public Author Author { get; set; }
+        public int PublisherId { get; set; }
+        // Book has an Publisher
+        public Publisher Publisher { get; set; }
 
     }
 }
---
+++ CS321_W3D1_BookAPI/Models/Publisher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CS321_W3D1_BookAPI.Models
+{
+    public class Publisher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int FoundedYear { get; set; }
+        public string CountryOfOrigin { get; set; }
+        public string HeadQuartersLocation { get; set; }
+        public ICollection<BookModel> Books { get; set; }
+    }
+}
